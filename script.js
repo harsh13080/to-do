59d8281dcf7448b8f283e87d04764c2608408197
Harsh Yadav <yh7864473@gmail.com>
2025-03-19 10:36:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function AddTask(){
+
+    if(inputBox.value === ''){
+        alert("You must write Something")
+    } else{
+
+        let li =document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        // cross
+        let span = document.createElement("span");
+        span.innerHTML= "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("cheked");
+        saveData();
+    } else if(e.target.tagName ==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+
+// save data
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
